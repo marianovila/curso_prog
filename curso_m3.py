45e698a555c9b6b9159ddcfdabb454ada894a83f
marianovila <mvila.cba@gmail.com>
2022-06-25 13:53:39 -0300

    2506

--- curso_m3.py
+++ curso_m3.py
@@ -1,4 +1,48 @@
-rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
 
-print(rooms)
-print("")
+"""
+cubed = [num ** 3 for num in range(5)]
+print(cubed)  # outputs: [0, 1, 8, 27, 64]
+
+table = [[":(", ":)", ":(", ":)"],
+         [":)", ":(", ":)", ":)"],
+         [":(", ":)", ":)", ":("],
+         [":)", ":)", ":)", ":("]]
+
+print(table)
+print(table[0][0])  # outputs: ':('
+print(table[0][3])  # outputs: ':)'
+
+
+tabla=[[1,2,3,4,5],
+[6,7,8,9,10],
+[11,12,13,14,15]]
+print(tabla)
+
+for i in range(-1,1):
+    print("#")
+    
+
+z=10
+y=0
+x=z>y or z==y
+print(x)
+
+
+my_list=[3,1,-1]
+my_list[-1]=my_list[-2]
+print(my_list)
+
+vals=[0,1,2]
+vals[0],vals[1]=vals[1],vals[2]
+print(vals)
+
+nums=[]
+vals=nums[:]
+vals.append(1)
+print(nums)
+print(vals)
+"""
+var=1
+while var<10:
+	print("#")
+    var=var<<1
---
+++ curso_m4.py
@@ -0,0 +1,33 @@
+
+""""
+
+
+
+def hello(a):    # definiendo una función
+    print("Hola,", a)    # cuerpo de la función
+
+
+name = input("Ingresa tu nombre: ")
+
+hello(name)    # invocación de la función
+"""
+
+""" AÑO BISIESTO
+def is_year_leap(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+        
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+	yr = test_data[i]
+	print(yr,"->",end="")
+	result = is_year_leap(yr)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Fallido")
+
+"""
